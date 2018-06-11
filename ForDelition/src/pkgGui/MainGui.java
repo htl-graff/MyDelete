82a0f4462b231ed1587abbd85afbc99b8b2231eb
htl-graff <39699412+htl-graff@users.noreply.github.com>
2018-06-11 13:48:32 +0200

    Update MainGui.java
    With console.log

--- ForDelition/src/pkgGui/MainGui.java
+++ ForDelition/src/pkgGui/MainGui.java
@@ -37,6 +37,7 @@ public class MainGui extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		Console.log("Hello")
 	}
 
 }
