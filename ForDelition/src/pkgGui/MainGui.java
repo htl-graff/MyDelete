bc5074635f71292093eb0e6fe1dec3bbf89b7ae1
htl-graff <schueler@ubuntu>
2018-06-11 04:52:42 -0700

    Main Gui without Console.log

--- ForDelition/src/pkgGui/MainGui.java
+++ ForDelition/src/pkgGui/MainGui.java
@@ -37,7 +37,6 @@ public class MainGui extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		Console.log("Hello")
 	}
 
 }
